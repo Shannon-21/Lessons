691ab773a888178fce4e758a1e147897cc0745af
Shannon-21 <fabiogiampaoli21@gmail.com>
2026-06-21 09:58:10 -0300

    lcc algos lists slist

---
+++ LCC/EstructurasDeDatosYAlgoritmos1/listas/slist/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "slist.h"
+
+static void imprimir_entero(int dato) {
+  printf("%d ", dato);
+}
+
+int main(int argc, char *argv[]) {
+
+  SList lista = slist_crear();
+
+  lista = slist_agregar_inicio(lista, 3);
+  lista = slist_agregar_inicio(lista, 2);
+  lista = slist_agregar_inicio(lista, 1);
+  lista = slist_agregar_final(lista, 4);
+
+  slist_recorrer(lista, imprimir_entero);
+  puts("");
+
+  imprimir_entero(slist_longitud(lista));
+  puts("");
+
+  slist_destruir(lista);
+
+  return 0;
+}
